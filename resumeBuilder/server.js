d30488ea527987cffcc2957e867f46bdce0d7f06
Seturathnam-S <seturathnam@hibizsolutions.com>
2021-06-09 15:24:18 +0530

    Update server.js

--- resumeBuilder/server.js
+++ resumeBuilder/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/resumeBuilder'));
+app.use(express.static('./dist/ResumeBuilder'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/resumeBuilder/'}),
+    res.sendFile('index.html', {root: 'dist/ResumeBuilder/'}),
 );
 
 // Start the app by listening on the default Heroku port

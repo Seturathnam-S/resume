4e1fd76a7c9b2efaba1bf90e15272a176eb66797
Seturathnam-S <seturathnam@hibizsolutions.com>
2021-06-10 13:04:55 +0530

    Update server.js

--- ResumeBuilder/server.js
+++ ResumeBuilder/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/resumeBuilder'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/public/index.html'));
+res.sendFile(path.join(__dirname+'/resumeBuilder/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
